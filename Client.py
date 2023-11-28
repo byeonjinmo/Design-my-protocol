fa4548225dd3708907cb7ec6a20e7b915d748f78
jin mo byeon <107828109+byeonjinmo@users.noreply.github.com>
2023-11-28 12:11:23 +0900

    가장 기본적인 소켓 통신의 클라이언트와 서버

--- Client.py
+++ Client.py
@@ -1,65 +1,60 @@
-# 6_server_thd
-# 다중 클라이언트로 부터 메시지를 받아서 브로드캐스트
-# 클라이언트가 exit 문자열이 보내올때까지 계속 수신
-# exit 문자열을 수신하면 while 문 탈출하여 연결종료
-
-from socket import *    # import socket 으로 하면 오류
-from select import *
+#client ID BR TO Q
+import socket
 from threading import Thread
+from datetime import datetime
 
-HOST = ''
-PORT = 10000
-BUFSIZE = 1024
-ADDR = (HOST, PORT)
+# server's IP address
+# if the server is not on this machine, 
+# put the private (network) IP address (e.g 192.168.1.2)
+SERVER_HOST = "127.0.0.1"
+SERVER_PORT = 5001 # server's port
+BUF_SIZE = 1024
+SEP = ":" # we will use this to separate the client name & message
 
-# 연결된 client의 소켓 집합 set of connected client sockets
-clientSockets = set()
+# initialize TCP socket
+s = socket.socket()
+print(f"[*] Connecting to {SERVER_HOST}:{SERVER_PORT}...")
+# connect to the server
+s.connect((SERVER_HOST, SERVER_PORT))
+print("[+] Connected.")
 
-# 소켓 생성
-serverSocket = socket(AF_INET, SOCK_STREAM)
-##     socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-
-# 소켓 주소 정보 할당
-serverSocket.bind(ADDR)
-print('바인드')
-
-# 연결 수신 대기 상태
-serverSocket.listen(10)
-print('대기')
-
-def client_com(cs):
-    # 클라이언트로부터 메시지를 가져옴
+def listen_for_messages():
     while True:
-        try:  # 아래 문장 무조건 실행
-            msg = cs.recv(BUFSIZE).decode()
-            #print('recieve data : ',msg)
-        except Exception as e:  # 위 문장 에러 처리: client no longer connected
+        try:
+            message = s.recv(BUF_SIZE).decode()
+            print("\n" + message)
+        except Exception as e:
             print(f"Error:{e}")
-            clientSockets.remove(cs)
-        else:  # 위 문장 에러 없을 시 실행
-            if msg == 'exit': # exit라는 메세지를 받으면 정상종료
-                cs.close()
-                clientSockets.remove(cs)
-                break
-            print('broadcast data : ',msg)
-            i=1;
-            for socket in clientSockets: # broadcast
-                socket.send(msg.encode())
-                print(i)
-                i=i+1
 
-# 연결 수락
+# make a thread that listens for messages to this client
+t = Thread(target=listen_for_messages)
+# make the thread daemon so it ends whenever the main thread ends
+t.daemon = True
+# start the thread
+t.start()
+
+# register of my ID to the Server
+myID = input("Enter your ID: ")
+to_Msg = "ID"+SEP+myID+SEP
+s.send(to_Msg.encode())
+
 while True:
-    clientSocekt, addr_info = serverSocket.accept()
-    print('연결 수락: client 정보 ', addr_info)
-    clientSockets.add(clientSocekt)
-    t = Thread(target = client_com, args=(clientSocekt,))
-    t.daemon = True
-    t.start()
+    # input message we want to send to the server
+    msg =  input()
+    tokens = msg.split(SEP)
+    code = tokens[0]
+    # a way to exit the program
+    if code.upper() == 'Q':
+        to_Msg = "Quit"+SEP+myID+SEP
+        s.send(to_Msg.encode())
+        break
+    elif code.upper()  == "BR" :
+        to_Msg = code + SEP + myID + SEP + tokens[1] + SEP
+        s.send(to_Msg.encode())
+    elif code.upper() == "TO":
+        to_Msg = code + SEP + myID + SEP + tokens[1] + SEP + tokens[2] + SEP
+        s.send(to_Msg.encode())
+    to_Msg = ''  # Initialization
 
-# 소켓 종료
-for cs in clientSockets:
-    cs.close()
-serverSocket.close()
-print('종료')
+# close the socket
+s.close()
---
+++ Server.py
@@ -0,0 +1,112 @@
+# server_thread dictionary exit
+# 클라이언트로 부터 exit 문자열이 올때까지 계속 수신
+# exit 문자열을 수신하면 while 문 탈출하여 연결종료
+
+from socket import *    
+from select import *
+from threading import Thread, Event
+
+event = Event()  # 키보드 입력받으면 키보드 종료시키기 위한 이벤트 
+
+HOST = ''
+PORT = 5001
+BUFSIZE = 1024
+ADDR = (HOST, PORT)
+
+# 연결된 client의 소켓 집합 set of connected client sockets
+clientSockets = {}
+       
+def msg_proc(cs, m):
+    global clientSockets
+    tokens = m.split(':')
+    code = tokens[0]
+    try:
+        if (code.upper() == "ID"):
+            print('reg id: ',m)
+            clientSockets[tokens[1]] = cs
+            cs.send("Success:Reg_ID".encode())
+            return True
+        elif (code.upper()  == "TO"):        
+            fromID = tokens[1]
+            toID = tokens[2]
+            toMsg = tokens[3]
+            print(f"1to1: From {fromID} To {toID} Message {toMsg}") 
+            toSocket = clientSockets.get(toID)
+            toSocket.send(m.encode())
+            cs.send("Success:1to1".encode())
+            return True
+        elif (code.upper()  == "BR"):
+            print('broadcast data: ', m)
+            for socket in clientSockets.values(): # broadcast
+                if (cs == socket):
+                    cs.send("Success:BR".encode())
+                else:
+                    socket.send(m.encode())
+            return True
+        elif (code.upper()  == "QUIT"):
+            fromID = tokens[1]
+            clientSockets.pop(fromID)
+            cs.close()
+            print("Disconnected:{}", fromID)
+            return False
+    except Exception as e:
+        print(f"Error:{e}")
+         
+def client_com(cs):
+    # 클라이언트로부터 id 메시지를 받음
+
+    while True:
+        if event.is_set(): # event 발생하면 스레드 종료
+            return
+        try:  # 아래 문장 무조건 실행
+            msg = cs.recv(BUFSIZE).decode()
+            #print('recieve data : ',msg)
+        except Exception as e:  # 위 문장 에러 처리: client no longer connected
+            print(f"Error:{e}")
+            clientSockets.pop(cs)
+        else:  # recv 성공하면 메시지 처리
+            if ( msg_proc(cs, msg) == False):
+                break  # 클라이언트가 종료하면 루프 탈출 후 스레드 종료
+        
+
+def client_acpt():
+    # 소켓 생성
+    global serverSocket 
+    serverSocket = socket(AF_INET, SOCK_STREAM) 
+
+    # 소켓 주소 정보 할당
+    serverSocket.bind(ADDR)
+
+    # 연결 수신 대기 상태
+    serverSocket.listen(10)
+    print('대기')
+
+    # 연결 수락
+    while True:
+        if event.is_set(): # event 발생하면 스레드 종료
+            return
+        clientSocket, addr_info = serverSocket.accept()
+        print('연결 수락: client 정보 ', addr_info)
+        tc = Thread(target = client_com, args=(clientSocket,))
+        tc.daemon = True
+        tc.start()
+        
+
+ta = Thread(target=client_acpt)
+ta.daemon = True
+ta.start()
+
+msg = input()
+if msg.upper() == "Q":
+    event.set()
+# 소켓 종료
+
+for socket in clientSockets.values():
+    try:
+        socket.shutdown()
+        socket.close()
+    except Exception as e:
+        continue
+    
+serverSocket.close()
+print('종료')
